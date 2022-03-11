b1d38754c1911a607a72339db261b47068748412
milk-maid <roqbell@gmail.com>
2022-03-10 19:27:41 -0800

    my first C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Print the phrase: "Programming is like building a multilingual puzzle
+ * Return: 0 if exiter properly. non-zero if otherwise
+ */
+
+int main(viod)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
