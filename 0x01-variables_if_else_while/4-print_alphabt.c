d63e8fd52829a81ec01933a1eef5183589f3695b
milk-maid <roqbell@gmail.com>
2022-03-11 15:34:33 -0800

    alphabets without e & q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the English alphabet without e or q
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
