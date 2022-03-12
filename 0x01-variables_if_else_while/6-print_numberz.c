4154e62ce4dcc476f8de3ca2213db1d6e5ab2949
milk-maid <roqbell@gmail.com>
2022-03-11 16:32:21 -0800

    why 38 and 48 and not other number range

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - printing zero to nine as in base ten
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
