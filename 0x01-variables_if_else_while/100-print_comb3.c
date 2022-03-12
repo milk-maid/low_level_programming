d829961f4ea199df01a2f17aa66dbac26041461a
milk-maid <roqbell@gmail.com>
2022-03-11 18:30:43 -0800

    ADVANCED

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - printing all possible diff combo of 2 digite
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n, m;
+
+	for (n = 48; n <= 58; n++)
+	{
+		for (m = 48; m <= 57; m++)
+		{
+			if (m < n)
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+
+			}
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
