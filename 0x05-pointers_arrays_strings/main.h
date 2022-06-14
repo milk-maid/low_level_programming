4e6ea5a2743e7b5a0de3c3e2b3b81eb08f216d7f
milk-maid <roqbell@gmail.com>
2022-06-13 19:06:17 -0700

    corrected the semicolon error in main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,5 +7,5 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 char *_strcpy(char *dest, char *src);
-void print_array(int *a, int n)
+void print_array(int *a, int n);
 int _atoi(char *s);
