031b7b328e1d754bbe0d0189904ccffb1db7aece
milk-maid <roqbell@gmail.com>
2022-06-25 02:45:15 +0000

    a script called create_static_lib.sh that creates a static library called liball.a from all the .c files that are in the current directory

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,15 @@
+ #include "main.h"
+
+/**
+ * _isupper - a function that checks for uppercase character
+ * @c: the checker to check
+ * Return: 1 for uppercase characters, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase letters
+ * @c: the VP is coming
+ * Return: 1 Success, 0 otherwise
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
