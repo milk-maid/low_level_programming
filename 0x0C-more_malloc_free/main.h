50beb6280d20422ba12f5288155e0ccd63ab8dd7
milk-maid <roqbell@gmail.com>
2022-07-03 11:22:09 +0000

    trying it out

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,5 +7,9 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void _is_zero(char *argv[]);
+int is_digit(char *s);
+int strlen(char *s);
+void errors(void);
+int main(int argc, char *argv[])
 
 #endif
