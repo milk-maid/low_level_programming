cff7fc2e061d1a57703a83709e7205b85eeb222e
milk-maid <roqbell@gmail.com>
2022-07-08 22:16:19 +0000

    only catroons

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -7,8 +7,8 @@
  *
  * Return: pointer to the function that corresponds to the operator.
  */
- int (*get_op_func(char *s))(int, int)
- {
+int (*get_op_func(char *s))(int, int)
+{
 	 op_t ops[] = {
 		 {"+", op_add},
 		 {"-", op_sub},
