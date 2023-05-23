5ca1223a845ad6a6e43e1ede593e9678f7d790d7
milk-maid <roqbell@gmail.com>
2023-05-23 21:45:30 +0100

    checks for alphabetic character.

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isalpha - checks for alphabetic character
+ * _isalpha - check for alphabetic character
  * @c: what to be checked
  * Return: 1 if c is a letter, 0 otherwisw
  */
