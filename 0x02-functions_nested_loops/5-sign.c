0d04ad96d42afbcf68b922cbc29a556fe0f721ce
milk-maid <roqbell@gmail.com>
2023-05-23 21:47:50 +0100

    prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - prints the sign of a number
  * @n: the int to check
- * Return: 1 and print + if n is greater than zero
+ * Return: 1 and print + if n is greatr than zero
  * 0 and print 0 if n is zero
  * -1 and print - if n is less than zero
  */
