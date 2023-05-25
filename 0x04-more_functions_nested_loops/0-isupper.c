00897bb83787b7ab64f275ae63e8254bc863be62
milk-maid <roqbell@gmail.com>
2023-05-25 22:30:40 +0100

    checks for uppercase character

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
  #include "main.h"
 
 /**
- * _isupper - a function that checks for uppercase character
+ * _isupper - a function that checks fr uppercase character
  * @c: the checker to check
  * Return: 1 for uppercase characters, 0 otherwise
  */
